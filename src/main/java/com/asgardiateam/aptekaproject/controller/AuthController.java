494266093bbd5ac3aaa6f2753c4e6173a8447d28
Abdurauf Vahobov <abduraufwahobov@gmail.com>
2023-05-04 11:13:14 +0500

    log out added

--- src/main/java/com/asgardiateam/aptekaproject/controller/AuthController.java
+++ src/main/java/com/asgardiateam/aptekaproject/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.asgardiateam.aptekaproject.controller;
 import com.asgardiateam.aptekaproject.config.security.jwt.JWTTokenProvider;
 import com.asgardiateam.aptekaproject.payload.AuthDTO;
 import com.asgardiateam.aptekaproject.payload.JwtTokenDTO;
+import com.asgardiateam.aptekaproject.payload.MessageDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,14 +12,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 import static com.asgardiateam.aptekaproject.common.ResponseData.ok;
+import static com.asgardiateam.aptekaproject.constants.MessageKey.SUCCESS_MESSAGE;
 import static com.asgardiateam.aptekaproject.constants.SecurityConstants.EXPIRATION_TIME;
 import static com.asgardiateam.aptekaproject.constants.SecurityConstants.TOKEN_PREFIX;
 
@@ -44,6 +43,11 @@ public class AuthController {
         String jwt = TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
 
         return  ok(new JwtTokenDTO(jwt, EXPIRATION_TIME, "Bearer"));
+    }
+
+    @GetMapping("/logout")
+    public Object logOut() {
+        return ok(new MessageDTO(SUCCESS_MESSAGE));
     }
 
 }
